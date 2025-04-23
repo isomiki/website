3186a16d3b9055ecdae59a5f414385fcb363b055
Marin <2474443-mrnb@users.noreply.gitlab.com>
2025-04-23 13:25:17 +0200

    setup with hotwire

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def projects
+  end
+
+  def tools
+  end
+
+  def contact
+  end
+
+  def posts
+  end
+
+  def crypto
+  end
+
+  def misc
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,43 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get projects" do
+    get pages_projects_url
+    assert_response :success
+  end
+
+  test "should get tools" do
+    get pages_tools_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+
+  test "should get posts" do
+    get pages_posts_url
+    assert_response :success
+  end
+
+  test "should get crypto" do
+    get pages_crypto_url
+    assert_response :success
+  end
+
+  test "should get misc" do
+    get pages_misc_url
+    assert_response :success
+  end
+end
